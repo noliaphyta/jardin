6937717e2efd2bdd848939ca76612d143c9b25fd
noliaphyta <115187646+noliaphyta@users.noreply.github.com>
2025-05-31 03:13:48 -0400

    remove publish

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,7 +23,7 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Caprasimo",
+        header: "Solway",
         body: "Sorts Mill Goudy",
         code: "Courier Prime ",
       },
@@ -75,7 +75,6 @@ const config: QuartzConfig = {
     ],
     filters: [
       Plugin.RemoveDrafts(),
-      Plugin.ExplicitPublish(),
     ],
     emitters: [
       Plugin.AliasRedirects(),
